cec050289797824f59ed71b95aa0655c2961d1a7
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-11 16:26:59 +0600

    updated cosden_solutions functionalities

--- cosden_solutions/src/components/Counter.tsx
+++ cosden_solutions/src/components/Counter.tsx
@@ -1,5 +1,27 @@
-//
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../state/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "../state/counter/counterSlice";
 
 export default function Counter() {
-  return <div>Counter</div>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  //
+  return (
+    <div>
+      <h2>{count}</h2>
+
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          Increment by amount 10
+        </button>
+      </div>
+    </div>
+  );
 }
